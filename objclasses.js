96b731143a89b888a6deed41800df70a322e5c0f
Ana Leticia Carvalho <analeticiacarvalho01@gmail.com>
2023-06-14 10:14:00 -0300

    classes

--- objclasses.js
+++ objclasses.js
@@ -1,16 +1,26 @@
-const pessoaa = {
-    nome: 'Leticia Carvalho',
-    idade: 29,
-    apresentar: function () {
-        console.log(`Meu nome é ${this.nome} tenho ${this.idade} anos e ${this.altura}cm de altura`)
+class Pessoa {
+    constructor(nome, idade, anoNascimento) {
+        this.nome = nome;
+        this.idade = idade;
+        this.anoNascimento = 2023 - idade;
+    }
+
+    descrever() {
+        console.log(`Meu nome é ${this.nome} e minha idade é ${this.idade}`);
     }
 }
 
-pessoa.altura = 150
+function comparaPessoas(p1, p2) {
+    if (p1.idade > p2.idade) {
+        console.log(`${p1.nome} é mais velho(a) que ${p2.nome}`);
+    } else if (p2.idade > p1.idade) {
+        console.log(`${p2.nome} é mais velho(a) que ${p1.nome}`);
+    } else {
+        console.log(`${p1.nome} e ${p2.nome} têm a mesma idade`);
+    }
+}
 
-pessoaa.apresentar()
+const leticia = new Pessoa('Letícia', 29);
+const jordania = new Pessoa('Jordânia', 30);
 
-//Assessando os atributos por string
-
-console.log(pessoa['nome'])
-
+comparaPessoas(leticia, jordania);
--- tempCodeRunnerFile.js
+++
@@ -1,1 +0,0 @@
-        console.log(`Meu nome é ${this.nome} tenho ${this.idade} anos e ${this.altura}cm de altura`)

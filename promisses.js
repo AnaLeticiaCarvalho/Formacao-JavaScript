199161472c8102ee359563e02e055e2cdee77302
Ana Leticia Carvalho <analeticiacarvalho01@gmail.com>
2023-09-13 22:12:32 -0300

    usando a extensão csv

--- promisses.js
+++ promisses.js
@@ -1,3 +1,13 @@
-new Promise((resolve, reject) => {
-    
+const numAleatorio = new Promise((resolve, reject) => {
+    const num = parseInt(Math.random() * 100)
+    resolve (num)
+})
+
+numAleatorio.then((value) => {
+    console.log(value)
+})
+
+    .catch((error) => console.error(error))
+    .finally( () => {
+    console.log('Finalizou')
 })

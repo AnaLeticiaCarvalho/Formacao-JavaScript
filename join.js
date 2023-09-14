ec4bdbe9ce92fce15711ffcdca753419526f8387
Ana Leticia Carvalho <analeticiacarvalho01@gmail.com>
2023-09-14 15:00:31 -0300

    manipulando promisses

--- join.js
+++ join.js
@@ -1,3 +1,15 @@
+const fs = require('fs')
+
+const promessaLeitura = fs.promises.readFile('tarefas.csv')
+
+promessaLeitura.then((arquivo) => {
+    console.log(arquivo.toString('utf8'))
+}).catch((error) => {
+    console.log('Deu ruim', error)
+})
+
+
+
 /*const list = [1, 2, 3]
 
 console.log(list.join('; '))*/
